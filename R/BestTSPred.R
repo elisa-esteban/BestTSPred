c3a0a4339010411a1ca5232ea0ec5df2870e604d
elisa-esteban <elisa.esteban.segurado@gmail.com>
2017-03-15 14:45:21 +0100

    Update BestTSPred

--- R/BestTSPred.R
+++ R/BestTSPred.R
@@ -43,22 +43,24 @@
 #'
 #' \dontrun{
 #' # With an object of class StQList
-#' data(StQList_Example)
+#' data(StQListExample)
 #' VarNames <- c('ActivEcono_35._6._2.1.4._0', 'GeoLoc_35._6._2.1._1.2.5.')
 #' TS.list <- list(Reg = list('RegDiffTSPred', forward = 2L),
 #'                 Stat = list('StatDiffTSPred', forward = 2L),
 #'                 StatReg = list('StatRegDiffTSPred', forward = 2L),
 #'                 Arima = list('AutoArimaTSPred', forward = 2L))
 #' BestTSPredParam <- new(Class='BestTSPredParam', TSPred.list = TS.list, VarNames = VarNames)
-#' BestTSPred(StQList_Example, BestTSPredParam)
+#' BestTSPred(StQListExample, BestTSPredParam)
 #' }
-#' @export
-setGeneric("BestTSPred", function(x, BestTSPredParam){standardGeneric("BestTSPred")})
-#' @rdname BestTSPred
 #'
-#' @import TSPred
+#' @import data.table RepoTime  StQ TSPred
 #'
 #' @include BestTSPredParam-class.R
+#'
+#' @export
+setGeneric("BestTSPred", function(x, BestTSPredParam){standardGeneric("BestTSPred")})
+
+#' @rdname BestTSPred
 #'
 #' @export
 setMethod(
@@ -89,29 +91,6 @@ setMethod(
 )
 #' @rdname BestTSPred
 #'
-#' @import TSPred
-#'
-#' @include BestTSPredParam-class.R
-#'
-#' @export
-setMethod(
-  f = "BestTSPred",
-  signature = c("matrix"),
-  function(x, BestTSPredParam){
-
-    output <- apply(x, 1, BestTSPred, BestTSPredParam)
-    output <- Reduce(rbind, output)
-    dimnames(output)[[1]] <- dimnames(x)[[1]]
-    return(output)
-
-  }
-)
-#' @rdname BestTSPred
-#'
-#' @import data.table StQ TSPred
-#'
-#' @include BestTSPredParam-class.R
-#'
 #' @export
 setMethod(
   f = "BestTSPred",
@@ -121,115 +100,42 @@ setMethod(
     VarNames <- BestTSPredParam@VarNames
     if (length(VarNames) == 0){
 
-      stop('[BestTSPred StQList] El slot VarNames del parámetro BestTSPredParam no puede estar vacío.')
+      stop('[BestTSPred StQList] Slot VarNames in the parameter BestTSPredParam must be specified.')
     }
 
-    QualsVal <- strsplit(VarNames, '_')
-    QualsVal <- lapply(QualsVal, function(Values){Values[2:length(Values)]})
+    if (length(VarNames) == 1){
 
-    OrigVarNames <- VarNames
-    VarNames <- ExtractNames(VarNames)
-    Data.list <- getData(x, VarNames)
-    IDQuals <- unlist(lapply(Data.list, getIDQual))
+      DT <- getValues(x, VarNames)
+      IDQuals <- setdiff(names(DT), c(VarNames, 'Period'))
+      DT[, orderPeriod := orderRepoTime(Period), by = IDQuals]
+      setkeyv(DT, c(IDQuals, 'orderPeriod'))
+      output <- DT[, BestTSPred(get(VarNames), BestTSPredParam = BestTSPredParam), by = IDQuals]
+      setnames(output, c('Pred', 'STD'), paste0(c('Pred', 'STD'), VarNames))
+      return(output)
 
-    DD <- getDD(Data.list[[length(Data.list)]])
-    Data.list <- lapply(Data.list, getData)
+    } else {
 
-    slotsNames <- names(getSlots('DD'))
-    slotsNames <- slotsNames[slotsNames != 'VarNameCorresp']
-    slotsDD <- lapply(slotsNames, function(x){slot(DD,x)})
-    DD <- DatadtToDT(Reduce('+', slotsDD))
+      DT.list <- lapply(VarNames, function(Var){
 
-    keyVar <- vector('list', length(VarNames))
-
-    for (i in 1:length(VarNames)){
-
-      key <- DD[Variable == VarNames[i]]
-      Quals <- setdiff(names(key), c('Variable', 'Sort', 'Class', 'Length', 'ValueRegExp'))
-      key <- transpose(key[, Quals, with = FALSE])[['V1']]
-      key <- key[key != '']
-      keyVar[[i]] <- setdiff(key, IDQuals)
-
-    }
-
-    Data.list <- lapply(seq(along = Data.list), function(index){
-
-      Data <- DatadtToDT(Data.list[[index]])
-      Data_Var <- lapply(seq(along = VarNames), function(var){
-
-        Quals <- QualsVal[[var]]
-        keys <- keyVar[[var]]
-
-        if (length(Quals) == length(keys)){
-
-          for (i in seq(along = keys)){
-
-            col <- keys[i]
-            Data <- Data[, aux := Data[[col]] == Quals[i]]
-            Data <- Data[aux == TRUE]
-            Data[, aux := NULL]
-          }
-
-        } else {
-
-          for (i in 1:(length(keys) - 1)){
-
-            col <- keys[i]
-            Data <- Data[, aux := Data[[col]] == Quals[i]]
-            Data <- Data[aux == TRUE]
-            Data[, aux := NULL]
-          }
-
-          Data <- Data[, aux := Data[[keys[length(keys)]]] == '']
-          Data <- Data[aux == TRUE]
-          Data[, aux := NULL]
-        }
-
-        return(Data)
+        LocalOutput <- getValues(x, Var)
+        setnames(LocalOutput, Var, 'Value')
+        LocalOutput[, Variable := Var]
+        return(LocalOutput)
       })
 
-      out <- rbindlist(Data_Var, fill = TRUE)
-      return(out)
-    })
-
-    UnitQuals <- names(getUnits(x[[length(x)]]))
-    keyVarTot <- unique(c(UnitQuals, unlist(keyVar)))
-    ValidUnits <- Data.list[[length(Data.list)]][, keyVarTot, with = F]
-    setkeyv(ValidUnits, keyVarTot)
-    ValidUnits <- ValidUnits[!duplicated(ValidUnits)]
-
-    Data.list <- lapply(Data.list, function(Data){
-
-      Data <- Data[, c(keyVarTot, 'IDDD', 'Value'), with = F]
-      setkeyv(Data, keyVarTot)
-      out <- Data[ValidUnits]
-      setkeyv(out, 'IDDD')
-      out <- out[VarNames]
-      return(out)
-
-    })
-
-    Data.list <- rbindlist(Data.list)
-    setkeyv(Data.list, c(keyVarTot, 'IDDD'))
-    Data.list[, Value := ifelse(Value == '', NA_real_, Value)]
-
-    output.DT <- Data.list[, lapply(.SD, BestTSPred, BestTSPredParam = BestTSPredParam),
-                           .SDcols = 'Value',
-                           by = setdiff(names(Data.list), 'Value')]
-
-    output.Pred <- output.DT[seq(1, dim(output.DT)[[1]], by = 2), c(UnitQuals, 'IDDD', 'Value'), with = F]
-    formulaPred <- as.formula(paste0(paste0(UnitQuals, collapse = ' + '), ' ~ IDDD'))
-    output.Pred <- dcast.data.table(output.Pred, formulaPred, value.var = 'Value')
-    setnames(output.Pred, VarNames, OrigVarNames)
-
-    output.STD <- output.DT[seq(2, dim(output.DT)[[1]], by = 2), c(UnitQuals, 'IDDD', 'Value'), with = F]
-    formulaSTD <- as.formula(paste0(paste0(UnitQuals, collapse = ' + '), ' ~ IDDD'))
-    output.STD <- dcast.data.table(output.STD, formulaSTD, value.var = 'Value')
-    setnames(output.STD, VarNames, OrigVarNames)
-
-    output <- list(Pred = output.Pred, STD = output.STD)
-
-    return(output)
+      DT <- rbindlist(DT.list)
+      IDQuals <- setdiff(names(DT), c('Variable', 'Period', 'Value'))
+      DT[, orderPeriod := orderRepoTime(Period), by = IDQuals]
+      setkeyv(DT, c(IDQuals, 'Variable', 'orderPeriod'))
+      output <- DT[, BestTSPred(Value, BestTSPredParam = BestTSPredParam), by = c(IDQuals, 'Variable')]
+      Form <- paste0(IDQuals, ' ~ Variable')
+      output.Pred <- dcast(output, as.formula(Form), value.var = 'Pred')
+      setnames(output.Pred, VarNames, paste0('Pred', VarNames))
+      output.STD <- dcast(output, as.formula(Form), value.var = 'STD')
+      setnames(output.STD, VarNames, paste0('STD', VarNames))
+      output <- merge(output.Pred, output.STD, by = IDQuals, all = TRUE)
+      return(output)
+    }
 
   }
 )
